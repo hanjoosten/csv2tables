1c228dec778a59c7b9e86b22acfa15ba2eaef9dd
hanjoosten <han.joosten.han@gmail.com>
2022-01-10 08:54:10 +0100

    analyseFileNames aangepast

--- src/Diacrieten.hs
+++ src/Diacrieten.hs
@@ -4,6 +4,7 @@ module Diacrieten (analyseFileNames) where
 
 import Import
 
+import qualified RIO.Text.Partial as T'
 import qualified RIO.Text as T
 import Data.Char
 
@@ -11,7 +12,19 @@ import Data.Char
 analyseFileNames :: RIO App ()
 analyseFileNames = do
   rows <- readFileUtf8 "FilesOpTschijf.txt"
-  writeFileUtf8 "AnalyseResultaat.txt" (T.unlines . mapMaybe analyseRow . T.lines $ rows)
+  let copyCommands = map toCopyCommand
+                   . mapMaybe analyseRow
+                   . T.lines 
+                   . T'.tail -- quick and dirty way to strip the BOM
+                   $ rows -- the stuff found in the file
+      contents = T.unlines (fileHeader: copyCommands)
+  writeFileUtf8 "AnalyseResultaat.txt" contents
+
+fileHeader :: Text
+fileHeader = T.unlines
+      ["$folderName = 'nasturen'+(Get-Date).tostring(\"dd-MM-yyyy\")"
+      ,"New-Item -ItemType Directory -Path Y:\\$FolderName"
+      ]
 
 analyseRow :: Text -> Maybe Text
 analyseRow row
@@ -23,3 +36,15 @@ analyseRow row
       isASdocument ('\\' : 'A' : 'S' : _) = True
       isASdocument [] = False
       isASdocument (_ : tl) = isASdocument tl
+
+toCopyCommand :: Text -> Text
+toCopyCommand txt =
+  "Copy-Item -Path '"
+  <>(T.pack . repairSingleQuotes . T.unpack $ txt)
+  <>"' -Destination Y:\\$folderName\\"
+  where
+    repairSingleQuotes :: String -> String
+    repairSingleQuotes [] = []
+    repairSingleQuotes (h:tl) = if h == '\'' 
+                                then h:h:repairSingleQuotes tl 
+                                else h:repairSingleQuotes tl
